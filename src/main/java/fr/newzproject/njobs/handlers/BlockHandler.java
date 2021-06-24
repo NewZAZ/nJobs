d8947d80e285d02f0692f28a85c243fff67ce950
BuildTools <unconfigured@null.spigotmc.org>
2021-06-24 13:51:29 +0200

    Update BlockHandler.java && BlockListeners.java

--- src/main/java/fr/newzproject/njobs/handlers/BlockHandler.java
+++ src/main/java/fr/newzproject/njobs/handlers/BlockHandler.java
@@ -6,11 +6,17 @@ import fr.newzproject.njobs.jobs.Jobs;
 import fr.newzproject.njobs.jobs.JobsManager;
 import fr.newzproject.njobs.jobs.enums.JobsEnum;
 import fr.newzproject.njobs.jobs.enums.JobsXPEnum;
+import net.minecraft.server.v1_12_R1.ChatMessageType;
+import net.minecraft.server.v1_12_R1.IChatBaseComponent;
+import net.minecraft.server.v1_12_R1.PacketPlayOutChat;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockBreakEvent;
+
+import java.io.IOException;
 
 public class BlockHandler {
 
@@ -29,13 +35,34 @@ public class BlockHandler {
         if(!SuperiorSkyblockAPI.getPlayer(player).hasIsland())return;
         if(SuperiorSkyblockAPI.getPlayer(player).getIsland() != SuperiorSkyblockAPI.getIslandAt(location))return;
 
-        if(block.hasMetadata(player.getUniqueId().toString()))return;
+        if(block.hasMetadata(player.getUniqueId().toString())){
+            block.removeMetadata(player.getUniqueId().toString(),plugin);
+            return;
+        }
 
         if(block.getType() != Material.CROPS && block.getType() != Material.CARROT && block.getType() != Material.POTATO && block.getType() != Material.NETHER_WARTS) {
             JobsEnum jobsEnum = JobsXPEnum.getMaterialJobs(block.getType());
 
             if(jobsEnum == null)return;
+            Jobs jobs = new Jobs(player, jobsEnum);
+            JobsManager jobsManager = new JobsManager(plugin,player);
             new JobsManager(plugin,player).addJobXp(jobsEnum, JobsXPEnum.getMaterialWorth(block.getType()));
+            sendActionbar(event.getPlayer(), "§a" + jobsEnum.getJobName() + ": " + jobsManager.getJobXp(jobsEnum) + "/" + JobsXPEnum.getXpForLevelup(jobs.getJobs()) + " (+" + JobsXPEnum.getMaterialWorth(event.getBlock().getType()) + ")");
+
+        }else {
+            if(JobsXPEnum.getMaterialData(event.getBlock().getType()) != event.getBlock().getData())return;
+            JobsEnum jobsEnum = JobsXPEnum.getMaterialJobs(event.getBlock().getType());
+            if (jobsEnum == null) return;
+            Jobs jobs = new Jobs(player, jobsEnum);
+            JobsManager jobsManager = new JobsManager(plugin,player);
+            jobsManager.addJobXp(jobsEnum,JobsXPEnum.getMaterialWorth(event.getBlock().getType()));
+            sendActionbar(event.getPlayer(), "§a" + jobsEnum.getJobName() + ": " + jobsManager.getJobXp(jobsEnum) + "/" + JobsXPEnum.getXpForLevelup(jobs.getJobs()) + " (+" + JobsXPEnum.getMaterialWorth(event.getBlock().getType()) + ")");
         }
+    }
+
+    public void sendActionbar(Player player, String message) {
+        IChatBaseComponent icbc = IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + message + "\"}");
+        PacketPlayOutChat packet = new PacketPlayOutChat(icbc, ChatMessageType.GAME_INFO);
+        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
     }
 }
--- src/main/java/fr/newzproject/njobs/listeners/BlockListeners.java
+++ src/main/java/fr/newzproject/njobs/listeners/BlockListeners.java
@@ -1,18 +1,39 @@
 package fr.newzproject.njobs.listeners;
 
+import com.bgsoftware.superiorskyblock.api.SuperiorSkyblockAPI;
+import com.bgsoftware.superiorskyblock.api.island.IslandPrivilege;
 import fr.newzproject.njobs.JobsCore;
 import fr.newzproject.njobs.handlers.BlockHandler;
+import fr.newzproject.njobs.jobs.enums.JobsEnum;
+import fr.newzproject.njobs.jobs.enums.JobsXPEnum;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.metadata.FixedMetadataValue;
+
+import java.util.Objects;
 
 public class BlockListeners implements Listener {
     private final JobsCore plugin;
 
     public BlockListeners(JobsCore plugin) {
         this.plugin = plugin;
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onBlockPlace(BlockPlaceEvent event){
+        if(event.isCancelled())return;
+        if(SuperiorSkyblockAPI.getIslandAt(event.getBlock().getLocation()) != null&& !Objects.requireNonNull(SuperiorSkyblockAPI.getIslandAt(event.getBlock().getLocation())).hasPermission(SuperiorSkyblockAPI.getPlayer(event.getPlayer()), IslandPrivilege.getByName("BUILD")))return;
+        JobsEnum eJobs = JobsXPEnum.getMaterialJobs(event.getBlock().getType());
+        if (eJobs == null) return;
+        if(event.getBlockPlaced().getType() == Material.CROPS || event.getBlockPlaced().getType() == Material.CARROT || event.getBlockPlaced().getType() == Material.POTATO || event.getBlockPlaced().getType() == Material.NETHER_WARTS)return;
+        if(JobsXPEnum.getMaterialJobs(event.getBlockPlaced().getType()) != null){
+            event.getBlockPlaced().setMetadata(event.getPlayer().getUniqueId().toString(),new FixedMetadataValue(plugin,true));
+        }
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
